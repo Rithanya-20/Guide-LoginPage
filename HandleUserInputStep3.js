3a72847e79189ca1b1881f64ba5016855cd5fbb2
Rithanya <124660123+Rithanya-20@users.noreply.github.com>
2025-03-24 12:12:40 +0530

    Create HandleUserInputStep3.js

---
+++ HandleUserInputStep3.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {Form, Button, Container, Row, Col} from 'react-bootstrap'
+import './Login.css';
+
+function Login() {
+
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('')
+    const [error, setError] = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Login attempted with: ',{email, password});
+        setError('')
+        if(!email || !password){
+            setError('Please enter the required fields')
+            console.log(error)
+            return;
+        }
+        console.log('Login attempted with:', { email, password });
+    }
+
+    return (
+       <div className='login-wrapper'>
+        <div className='login-form-container'>
+           
+            <h2 className='login-title'>Login</h2>
+            <Form onSubmit={handleSubmit}>
+            <Form.Group className='login-form' controlId='formBasicEmail'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type='email'
+                placeholder='Enter Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}/>
+            </Form.Group>
+
+            <Form.Group className='login-form' controlId='formBasicPassword'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type='password' 
+                placeholder='Enter Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}/>
+            </Form.Group>
+
+            <Button variant='primary' type='submit' className='login-button'>Login</Button>
+
+            </Form>
+           
+        </div>
+       </div>
+
+    );
+}
+
+export default Login;
+
